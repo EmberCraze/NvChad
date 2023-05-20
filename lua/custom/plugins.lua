62daf5f1f4f0322eedf05d1f69432f6c8bb19ea5
EmberCraze <maher.shaker@live.se>
2023-05-20 23:52:05 +0200

    add support for tabnine suggestions

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -41,7 +41,38 @@ local plugins = {
         "black",
       },
     },
-  }
+  },
+	{
+		"hrsh7th/nvim-cmp",
+		opts = {
+		  sources = {
+			{ name = "nvim_lsp" },
+			{ name = "luasnip" },
+			{ name = "buffer" },
+			{ name = "nvim_lua" },
+			{ name = "path" },
+			{ name = "cmp_tabnine" },
+		  },
+		},
+
+		dependencies = {
+		  {
+			"tzachar/cmp-tabnine",
+			build = "./install.sh",
+			config = function()
+			  local tabnine = require "cmp_tabnine.config"
+			  tabnine:setup {
+						max_lines = 1000,
+						max_num_results = 5,
+						sort = true,
+						run_on_every_keystroke = true,
+						snippet_placeholder = '..',
+						show_prediction_strength = false
+					} -- put your options here
+			end,
+		  },
+		},
+	},
 }
 
 return plugins
