26803a70f3b7ac3054325ec125a5f3fbee93a70f
EmberCraze <maher.shaker@live.se>
2023-09-16 12:00:12 +0200

    update tab mapping and add plugins

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -3,8 +3,34 @@ local M = {}
 M.disabled = {
   n = {
       ["<leader>h"] = "",
-    --["<C-a>"] = ""
+      ["<leader>q"] = "",
+	  ["<C-n>"] = "",
   }
+}
+
+M.nvimtree = {
+  n = {
+	  ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
+  },
+}
+
+M.tabufline = {
+  n = {
+    -- close buffer + hide terminal buffer
+    ["<leader>qb"] = {
+      function()
+        require("nvchad.tabufline").close_buffer()
+      end,
+      "Close buffer",
+    },
+
+    ["<leader>qa"] = {
+      function()
+        require("nvchad.tabufline").closeAllBufs()
+      end,
+      "Close buffer",
+    },
+  },
 }
 
 return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -34,6 +34,7 @@ local plugins = {
      opts = {
       ensure_installed = {
         "lua-language-server",
+		"typescript-language-server",
         "html-lsp",
         "prettier",
         "stylua",
@@ -73,6 +74,16 @@ local plugins = {
 		  },
 		},
 	},
+	{
+		'glacambre/firenvim',
+
+		-- Lazy load firenvim
+		-- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+		lazy = not vim.g.started_by_firenvim,
+		build = function()
+			vim.fn["firenvim#install"](0)
+		end
+	}
 }
 
 return plugins
