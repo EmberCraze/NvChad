50b3f14b0255af6261764f8e35d37eed5891d606
embercraze <maher.shaker@live.se>
2023-09-24 20:05:53 +0200

    feat: cofigure null-ls to format on save
    I wanted black to format my python files every time i saved so I added this

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -2,6 +2,7 @@ local null_ls = require "null-ls"
 
 local formatting = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local sources = {
    formatting.prettier,
@@ -14,4 +15,18 @@ local sources = {
 null_ls.setup {
    debug = false,
    sources = sources,
+	on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+						-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+						vim.lsp.buf.format({ async = false })
+				end,
+				})
+			end
+		end,
 }
