3e16233fd16407ff6b00a66006c9736fce566487
embercraze <maher.shaker@live.se>
2023-09-24 20:04:55 +0200

    feat: configure firenvim to autosave and not auto claim element

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -82,8 +82,32 @@ local plugins = {
 		lazy = not vim.g.started_by_firenvim,
 		build = function()
 			vim.fn["firenvim#install"](0)
-		end
-	}
+		end,
+		config = function()
+			-- For auto save to buffer
+			vim.api.nvim_create_autocmd({'TextChanged', 'TextChangedI'}, {
+				callback = function(e)
+					if vim.g.timer_started == true then
+						return
+					end
+					vim.g.timer_started = true
+					vim.fn.timer_start(10000, function()
+						vim.g.timer_started = false
+						vim.cmd('write')
+					end)
+				end
+			})
+
+			vim.g.firenvim_config = {
+			  localSettings = {
+				[".*"] = {
+				  takeover = "never",
+				  cmdline = "firenvim",
+				},
+			  },
+			}
+		end,
+	},
 }
 
 return plugins
