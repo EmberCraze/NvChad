a8be733481848346c94051123f929f8b0cc57882
embercraze <maher.shaker@live.se>
2024-11-05 16:19:13 +0100

    refactor: delete unused files

--- backup/configs/gigapay/start_debug.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker compose -f ~/gigapay_dev/gigapay/local.yml -f ~/gigapay_dev/gigapay/.nvim/override_debug.yml up --exit-code-from django --abort-on-container-exit django
--- backup/configs/gigapay/start_debug_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ]; then
-  docker-compose -f ~/gigapay_dev/gigapay/local.yml -f ~/gigapay_dev/gigapay/.nvim/override_debug.yml run --no-deps --rm django python manage.py test --noinput --parallel 8 --exclude-tag=integration --settings=config.settings.test
-else 
-  docker-compose -f ~/gigapay_dev/gigapay/local.yml -f ~/gigapay_dev/gigapay/.nvim/override_debug.yml run --no-deps --rm django python manage.py test --noinput --parallel 8 --exclude-tag=integration --settings=config.settings.test --keepdb "$1"
-fi
